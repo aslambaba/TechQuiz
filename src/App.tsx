c9bc17d6bc6d90dc76154552a8e0385e87803def
Aslam Sarfraz <aslam91r@gmail.com>
2020-11-25 01:12:17 +0500

    SubmitHandler Error Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
 import { useEffect } from 'react';
-import { JsxFlags } from 'typescript';
-import {ApiQuiz,DevQuiz} from './Types/quizTypes';
+import { useState } from 'react';
+import { ApiQuiz, DevQuiz } from './Types/quizTypes';
 
 function App() {
 
-  const suffle = (array: any[]) =>{
-    return [...array].sort(()=> Math.random() - 0.5);
-  };
-  useEffect(()=>{
+  const [quiz, setQuiz] = useState<DevQuiz[]>([]);
+  let [counter, setCounter] = useState(0);
 
+  const suffle = (array: any[]) => {
+    return [...array].sort(() => Math.random() - 0.5);
+  };
+
+  useEffect(() => {
     async function getQuizAPI() {
       const api = await fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple');
-      const {results} = await api.json();
-      const apiData:DevQuiz[] = await results.map((obj: ApiQuiz) => {
+      const { results } = await api.json();
+      const apiData: DevQuiz[] = await results.map((obj: ApiQuiz) => {
         return {
           question: obj.question,
           correct_answer: obj.correct_answer,
@@ -25,27 +27,35 @@ function App() {
       setQuiz(apiData);
     }
     getQuizAPI();
+
   })
 
-  const nextBTN = ()=>{
-    setCounter(++counter);
+  const nextBTN = (e: any) => {
+    e.preventDefault();
+    if (counter < 9) {
+      console.log(counter)
+      setCounter(++counter);
+    } else {
+
+      console.log('EEEE')
+      setCounter(0);
+    }
   }
-  const [quiz,setQuiz] = useState<DevQuiz[]>([]);
 
-  let [counter,setCounter] = useState(0);
 
-  if(quiz.length ==  0){
+  if (quiz.length === 0) {
     return <h1>Loading...</h1>
   }
-  else{
+  else {
     return (
       <div>
         <p>{quiz[counter].question}</p>
-        {quiz[counter].options.map(obj=>{
-          return(
-          <button onClick={nextBTN}>{obj}</button>
+        {quiz[counter].options.map((obj:String) => {
+          return (
+            <button>{obj}</button>
           )
-        })}     
+        })}
+        <button onClick={nextBTN}> Submit </button>
       </div>
     );
   }
